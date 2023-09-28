096a2a758ade443c79fa5e2b16e13fbcc106dec7
tilaksingh <tilaks484@gmail.com>
2023-09-27 19:47:38 -0600

    ARCANE

--- ooad/src/main/java/ooad/arcane/Main.java
+++ ooad/src/main/java/ooad/arcane/Main.java
@@ -3,6 +3,24 @@ package ooad.arcane;
 public class Main {
     public static void main(String[] args) {
 
+        // ARCANE - RPG !!!
+
+        GameDriver current_game = new GameDriver();
+        BoardRenderer boardRenderer = new BoardRenderer();
+        for(int turn_number=1; turn_number<=5;turn_number++) {
+            // Move Adventurers
+            current_game.move_adventurers();
+            // Move creatures
+            current_game.move_creatures();
+            current_game.update_adventurer_attributes();
+            // Combat or Treasure condition
+            current_game.check_adventurer_creature_same_room(current_game.active_adventurers);
+            // Game termination condition
+            if(current_game.isGameOver())
+                break;
+            // Printing the board
+            boardRenderer.print_game(turn_number, current_game);
+        }
 
 //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SingleGameRun.txt ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 //        GameDriver current_game = new GameDriver();
