6e964411a722b8ce2f93a67ae5e9a80d29ba4bd1
Jayant-Duneja <jayant.duneja@colorado.edu>
2023-10-10 23:23:35 -0600

    Element

--- src/com/arcane/Element.java
+++ src/com/arcane/Element.java
@@ -1,14 +1,14 @@
 package com.arcane;
 
 public enum Element {
-  FIRE("fire"),
-  WATER("water"),
-  EARTH("earth"),
-  AIR("air");
+    FIRE("fire"),
+    WATER("water"),
+    EARTH("earth"),
+    AIR("air");
 
-  private final String type;
+    private final String type;
 
-  Element(String type) {
-    this.type = type;
-  }
+    Element(String type) {
+        this.type = type;
+    }
 }
