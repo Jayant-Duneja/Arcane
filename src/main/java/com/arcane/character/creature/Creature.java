3585eb2c436a3ef31ae3cd4973425ab6d1f42dd8
Jayant-Duneja <jayant.duneja@colorado.edu>
2023-10-25 13:51:13 -0600

    Game Commit-1

--- src/main/java/com/arcane/character/creature/Creature.java
+++ src/main/java/com/arcane/character/creature/Creature.java
@@ -38,10 +38,10 @@ public abstract class Creature extends Character {
     return acronym;
   }
 
-  @Override
-  protected void fight(GameBoard gameBoard, ConcreteSubject concreteSubject) {
-    fightAdventures(gameBoard, concreteSubject);
-  }
+//  @Override
+//  protected void fight(GameBoard gameBoard, ConcreteSubject concreteSubject) {
+//    fightAdventures(gameBoard, concreteSubject);
+//  }
   public void performTurn(GameBoard gameBoard, ConcreteSubject concreteSubject){
     if (!isFightScenario(gameBoard)) {
       move(gameBoard, concreteSubject);
@@ -55,36 +55,36 @@ public abstract class Creature extends Character {
     gameBoard.getRoom(currentRoomId).addCreature(this);
     concreteSubject.add_event_to_current_turn(EventType.Creature_enter_room, this.acronym.acronym, currentRoomId,gameBoard);
   }
+//
+//  @Override
+//  protected void postMove(GameBoard gameBoard, ConcreteSubject concreteSubject) {
+//    if (isFightScenario(gameBoard)) {
+//      fightAdventures(gameBoard, concreteSubject);
+//    }
+//  }
 
-  @Override
-  protected void postMove(GameBoard gameBoard, ConcreteSubject concreteSubject) {
-    if (isFightScenario(gameBoard)) {
-      fightAdventures(gameBoard, concreteSubject);
-    }
-  }
-
-  protected void fightAdventures(GameBoard gameBoard, ConcreteSubject concreteSubject) {
-    for (Adventurer adventurer : gameBoard.getRoom(this.currentRoomId).getAdventurers()) {
-      // Fight if adventurer is alive
-      if (adventurer.isAlive()) {
-        if (this.combatRoll() > adventurer.combatRoll()) {
-          if (!adventurer.isDodgeSuccessful()) {
-            concreteSubject.add_event_to_current_turn(EventType.LOSE_HEALTH, adventurer.getAcronym().acronym + "-" + adventurer.getCreatureDamage());
-            adventurer.takeDamage();
-            if(!adventurer.isAlive()){
-              concreteSubject.add_event_to_current_turn(EventType.DEFEAT, adventurer.getAcronym().acronym);
-            }
-          }
-        } else if (adventurer.combatRoll() > this.combatRoll()) {
-          // if creature loses then remove from the current room and end fighting
-          gameBoard.getRoom(this.currentRoomId).removeCreature(this);
-          concreteSubject.add_event_to_current_turn(EventType.WIN_COMBAT, adventurer.getAcronym().acronym);
-          concreteSubject.add_event_to_current_turn(EventType.LOSE_COMBAT, this.getAcronym().acronym);
-          concreteSubject.add_event_to_current_turn(EventType.DEFEAT, this.getAcronym().acronym);
-          break;
-        }
-      }
-    }
-  }
+//  protected void fightAdventures(GameBoard gameBoard, ConcreteSubject concreteSubject) {
+//    for (Adventurer adventurer : gameBoard.getRoom(this.currentRoomId).getAdventurers()) {
+//      // Fight if adventurer is alive
+//      if (adventurer.isAlive()) {
+//        if (this.combatRoll() > adventurer.combatRoll()) {
+//          if (!adventurer.isDodgeSuccessful()) {
+//            concreteSubject.add_event_to_current_turn(EventType.LOSE_HEALTH, adventurer.getAcronym().acronym + "-" + adventurer.getCreatureDamage());
+//            adventurer.takeDamage();
+//            if(!adventurer.isAlive()){
+//              concreteSubject.add_event_to_current_turn(EventType.DEFEAT, adventurer.getAcronym().acronym);
+//            }
+//          }
+//        } else if (adventurer.combatRoll() > this.combatRoll()) {
+//          // if creature loses then remove from the current room and end fighting
+//          gameBoard.getRoom(this.currentRoomId).removeCreature(this);
+//          concreteSubject.add_event_to_current_turn(EventType.WIN_COMBAT, adventurer.getAcronym().acronym);
+//          concreteSubject.add_event_to_current_turn(EventType.LOSE_COMBAT, this.getAcronym().acronym);
+//          concreteSubject.add_event_to_current_turn(EventType.DEFEAT, this.getAcronym().acronym);
+//          break;
+//        }
+//      }
+//    }
+//  }
   public String getCurrentRoomId(){return this.currentRoomId;}
 }
